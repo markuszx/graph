bd200f6c4a08a75760a9a4c45f39e56f939d72db
mark garcia <markkusag@outlook.com>
2024-02-13 23:58:23 -0600

    unfinish problem 4

--- graphs.js
+++ graphs.js
@@ -18,40 +18,40 @@ function printBreadthFirst(start) {
 
 
     // Create a queue and enqueue the starting node
-      let q = [start]
+    let q = [start]
     //Create a set to store visited nodes
     let visited = new Set().add(start)
-   //While the queue is not empty, repeat steps 4-6
-    while(q.length > 0){
+    //While the queue is not empty, repeat steps 4-6
+    while (q.length > 0) {
 
         //Dequeue the first node
-    let dequeued = q.shift()
-    //DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
-    console.log(dequeued)
+        let dequeued = q.shift()
+        //DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
+        console.log(dequeued)
 
-    //For each unvisited neighbor, add it to the visited nodes and to the back of the queue
+        //For each unvisited neighbor, add it to the visited nodes and to the back of the queue
 
-   adjList1[dequeued].forEach(node => {
-       if(!visited.has(node)){
-        q.push(node)
-        visited.add(node)
-       }
+        adjList1[dequeued].forEach(node => {
+            if (!visited.has(node)) {
+                q.push(node)
+                visited.add(node)
+            }
 
-   });
+        });
     }
 
 
 }
 
-console.log("First Test:")
-printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
-// One possible solution:  3, 2, 4, 1, 5, 6
-console.log("Second Test:")
-printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
-// One possible solution:  6, 4, 3, 5, 2, 1
-console.log("Third Test:")
-printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
-// One possible solution:  4, 3, 5, 6, 2, 1
+// console.log("First Test:")
+// printBreadthFirst(3); // Prints 1 through 6 in Breadth-first order, starting with 3
+// // One possible solution:  3, 2, 4, 1, 5, 6
+// console.log("Second Test:")
+// printBreadthFirst(6); // Prints 1 through 6 in Breadth-first order, starting with 6
+// // One possible solution:  6, 4, 3, 5, 2, 1
+// console.log("Third Test:")
+// printBreadthFirst(4); // Prints 1 through 6 in Breadth-first order, starting with 4
+// // One possible solution:  4, 3, 5, 6, 2, 1
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -72,35 +72,35 @@ function printDepthFirst(start) {
     let stack = [start]
     //Create a set to store visited nodes
     let visited = new Set().add(start)
-   //While the queue is not empty, repeat steps 4-6
-    while(stack.length > 0){
+    //While the queue is not empty, repeat steps 4-6
+    while (stack.length > 0) {
 
         //Dequeue the first node
-    let popped = stack.pop()
-    //DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
-    console.log(popped)
+        let popped = stack.pop()
+        //DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
+        console.log(popped)
 
-    //For each unvisited neighbor, add it to the visited nodes and to the back of the queue
+        //For each unvisited neighbor, add it to the visited nodes and to the back of the queue
 
-   adjList2[popped].forEach(node => {
-       if(!visited.has(node)){
-       stack.push(node)
-        visited.add(node)
-       }
+        adjList2[popped].forEach(node => {
+            if (!visited.has(node)) {
+                stack.push(node)
+                visited.add(node)
+            }
 
-   });
+        });
     }
 }
 
-console.log("First Test:")
-printDepthFirst(3); // Prints 1 through 6 in Depth-first order, starting with 3
-// One possible solution:  3, 4, 6, 5, 1, 2
-console.log("Second Test:")
-printDepthFirst(6); // Prints 1 through 6 in Depth-first order, starting with 6
-// One possible solution:  6, 4, 5, 2, 1, 3
-console.log("Third Test:")
-printDepthFirst(4); // Prints 1 through 6 in Depth-first order, starting with 4
-// One possible solution:  4, 6, 5, 2, 1, 3
+// console.log("First Test:")
+// printDepthFirst(3); // Prints 1 through 6 in Depth-first order, starting with 3
+// // One possible solution:  3, 4, 6, 5, 1, 2
+// console.log("Second Test:")
+// printDepthFirst(6); // Prints 1 through 6 in Depth-first order, starting with 6
+// // One possible solution:  6, 4, 5, 2, 1, 3
+// console.log("Third Test:")
+// printDepthFirst(4); // Prints 1 through 6 in Depth-first order, starting with 4
+// // One possible solution:  4, 6, 5, 2, 1, 3
 
 
 
@@ -123,34 +123,34 @@ function breadthFirstSearch(start, end) {
     let q = [start]
     //Create a set to store visited nodes
     let visited = new Set().add(start)
-   //While the queue is not empty, repeat steps 4-6
-    while(q.length > 0){
+    //While the queue is not empty, repeat steps 4-6
+    while (q.length > 0) {
 
         //Dequeue the first node
-    let dequeued = q.shift()
-    //DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
-    if(dequeued === end)return true;
+        let dequeued = q.shift()
+        //DO THE THING THAT YOU NEED TO FOR THE DEQUEUED NODE
+        if (dequeued === end) return true;
 
-    //For each unvisited neighbor, add it to the visited nodes and to the back of the queue
+        //For each unvisited neighbor, add it to the visited nodes and to the back of the queue
 
-   adjList3[dequeued].forEach(node => {
-       if(!visited.has(node)){
-        q.push(node)
-        visited.add(node)
-       }
+        adjList3[dequeued].forEach(node => {
+            if (!visited.has(node)) {
+                q.push(node)
+                visited.add(node)
+            }
 
-   });
+        });
     }
     return false
 
 }
 
-console.log("First Test:");
-console.log(breadthFirstSearch(1, 3)); // -> true
-console.log("Second Test:");
-console.log(breadthFirstSearch(4, 1)); // -> true
-console.log("Third Test:");
-console.log(breadthFirstSearch(6, 1)); // -> false
+// console.log("First Test:");
+// console.log(breadthFirstSearch(1, 3)); // -> true
+// console.log("Second Test:");
+// console.log(breadthFirstSearch(4, 1)); // -> true
+// console.log("Third Test:");
+// console.log(breadthFirstSearch(6, 1)); // -> false
 
 
 
@@ -170,13 +170,44 @@ const adjList4 = {
 
 function aShortestPath(start, end) {
     // your code here
+    //1. Create a queue. Create a separate array containing the starting node. Enqueue this array. The enqueued array is the current path.
+    let q =[[start]]
+
+    //2.Create a set to store visited nodes
+    let visited = new Set().add(start)
+
+    //3.While the queue is not empty, repeat steps 4-6
+    while(q.length >0){
+
+        //4.Dequeue the first path, and save it in a variable
+        let dequeuedPath = q.shift();
+        //5.Save the last node in the path in a variable (DO NOT pop it)
+        let lastNode = dequeuedPath[dequeuedPath.length-1]
+        //6.IS THIS NODE THE THING? If so, stop and return a result. Else, continue.
+        if(dequeuedPath === end)return dequeuedPath;
+
+          //1.For each unvisited neighbor of the last node:
+          adjList4[lastNode].forEach(node => {
+            if(!visited.has(node)){
+             let newPath = [...dequeuedPath, [node]];
+              // dequeuedPath.push([node])
+               q.push(newPath)
+                //2.Add it to the visited nodes set
+                visited.add(node)
+            }
+          })
+
+    }
+    return false
+    //7. Copy the saved path, and add the neighbor to the end. Enqueue this new path
+    //8. If the queue has become empty without finding the thing, then the thing has not been found. Return false, an error, or a message as appropriate for the problem you are solving.
 }
 
-console.log("First Test:");
+console.log("First Test 4:");
 console.log(aShortestPath(1, 3)); // -> [ 1, 2, 3 ] (One possible solution)
-console.log("Second Test:");
+console.log("Second Test 4:");
 console.log(aShortestPath(4, 1)); // -> [ 4, 5, 1 ] (One possible solution)
-console.log("Third Test:");
+console.log("Third Test 4:");
 console.log(aShortestPath(6, 1)); // -> false
 
 
@@ -198,9 +229,9 @@ function degreesOfSeparation(start, end) {
     // your code here
 }
 
-console.log("First Test:");
-console.log(degreesOfSeparation(1, 3)); // -> 2
-console.log("Second Test:");
-console.log(degreesOfSeparation(5, 2)); // -> 1
-console.log("Third Test:");
-console.log(degreesOfSeparation(6, 1)); // -> false
+// console.log("First Test:");
+// console.log(degreesOfSeparation(1, 3)); // -> 2
+// console.log("Second Test:");
+// console.log(degreesOfSeparation(5, 2)); // -> 1
+// console.log("Third Test:");
+// console.log(degreesOfSeparation(6, 1)); // -> false
